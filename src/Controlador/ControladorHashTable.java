6ca346900b6d7e7578c38a6aa66d21989be4d517
erickcamposeco <ecamposecog@miumg.edu.gt>
2025-06-03 23:54:49 -0600

    controlador

--- src/Controlador/ControladorHashTable.java
+++ src/Controlador/ControladorHashTable.java
@@ -1,6 +1,52 @@
 
 package Controlador;
 
+import Vista.VistaPrincipal;
+import javax.swing.*;
+import java.awt.*;
+
+
 public class ControladorHashTable {
+    
+    private HashTable hashTable;
+    private VistaPrincipal vista;
+    
+    public ControladorHashTable (VistaPrincipal vista){
+        this.vista = vista;
+        this.hashTable = new HashTable(10);
+    }
+    
+     public void mostrarTablaHash() //Mostrar la hashtable
+    {
+        JPanel panelHash = new JPanel(new BorderLayout());
+        JTextArea inputArea = new JTextArea(10, 20);
+        JTextArea outputArea = new JTextArea(10, 20);
+        outputArea.setEditable(false);
+
+        inputArea.addKeyListener(new java.awt.event.KeyAdapter() //Los listeners para la hashtable
+        {
+            public void keyReleased(java.awt.event.KeyEvent evt) 
+            {
+                String[] lines = inputArea.getText().split("\n");
+                StringBuilder output = new StringBuilder();
+                for (String line : lines) {
+                    int hashValue = hashTable.hash(line);
+                    hashTable.put(line, hashValue);
+                    output.append(line).append(" -> ").append(hashValue).append("\n");
+                }
+                outputArea.setText(output.toString());
+            }
+        });
+        //Adicion del os panelHash para la vista
+        panelHash.add(new JScrollPane(inputArea), BorderLayout.WEST);
+        panelHash.add(new JScrollPane(outputArea), BorderLayout.EAST);
+        vista.agregarHoja("Tabla Hash", panelHash);
+    }
+
+    public void agregarDato(String key) 
+    {
+        hashTable.put(key, hashTable.hash(key));
+        // Código para actualizar la vista de la tabla hash
+    }
     
 }
--- src/Controlador/HashTable.java
+++ src/Controlador/HashTable.java
@@ -1,14 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controlador;
 
-/**
- *
- * @author Erick Camposeco
- */
+import java.util.LinkedList;
+
 public class HashTable {
     
+    private class Entry {
+         String key;
+        int value;
+        Entry next;
+
+        Entry(String key, int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+    
+    private LinkedList<Entry>[] table;
+    private int size;
+
+    public HashTable(int size) 
+    {
+        this.size = size;
+        table = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    int hash(String key) 
+    {
+        int hash = 0;
+        for (char c : key.toCharArray()) {
+            hash = (hash * 31 + c) % size;
+        }
+        return hash;
+    }
+
+    public void put(String key, int value) 
+    {
+        int index = hash(key);
+        for (Entry entry : table[index]) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+        }
+        table[index].add(new Entry(key, value));
+    }
+
+    public Integer get(String key) 
+    {
+        int index = hash(key);
+        for (Entry entry : table[index]) {
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
+        return null;
+    }
     
 }
