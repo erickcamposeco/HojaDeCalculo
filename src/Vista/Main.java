aa19ff2eed22a132d2f449250f029cffdd3da4ce
Carlos Emanuel Del Cid Corado <163491622+Caremaedu@users.noreply.github.com>
2025-06-04 20:18:03 -0600

    Menu y VIsta de la hoja de calculo

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,10 +1,16 @@
 
 package Vista;
 
-/**
- *
- * @author Erick Camposeco
- */
+//Controlador de la vista
+
+import Controlador.ControladorHojaCalculo;
+import hoja_calculo.Libro;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        VistaPrincipal vista = new VistaPrincipal();
+        Libro libro = new Libro();
+        ControladorHojaCalculo controlador = new ControladorHojaCalculo(vista, libro);
+    }
 }
--- src/Vista/VistaPrincipal.java
+++ src/Vista/VistaPrincipal.java
@@ -1,10 +1,53 @@
 
 package Vista;
+//Vista
 
-/**
- *
- * @author Erick Camposeco
- */
-public class VistaPrincipal {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class VistaPrincipal 
+{
+
+    private JFrame frame;
+    private JTabbedPane tabbedPane;
+
+    public VistaPrincipal() {
+        frame = new JFrame("Hoja de Calculo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu archivoMenu = new JMenu("Archivo");
+        JMenuItem nuevaHojaItem = new JMenuItem("Nueva Hoja");
+        JMenuItem tablaHashItem = new JMenuItem("Tabla Hash");
+        archivoMenu.add(nuevaHojaItem);
+        archivoMenu.add(tablaHashItem);
+        menuBar.add(archivoMenu);
+        frame.setJMenuBar(menuBar);
+
+        tabbedPane = new JTabbedPane();
+        frame.add(tabbedPane);
+
+        frame.setVisible(true);
+    }
+
+    public void agregarHoja(String nombre, JPanel panel) {
+        tabbedPane.addTab(nombre, panel);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    public JTabbedPane getTabbedPane() {
+        return tabbedPane;
+    }
+
+    public JMenuItem getNuevaHojaItem() {
+        return ((JMenu) frame.getJMenuBar().getMenu(0)).getItem(0);
+    }
+
+    public JMenuItem getTablaHashItem() {
+        return ((JMenu) frame.getJMenuBar().getMenu(0)).getItem(1);
+    }
 }
