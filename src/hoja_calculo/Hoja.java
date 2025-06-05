c3ed868f635fa8bdf31892d17292aaf9b912dd43
Wilbrayan <btunchea@miumg.edu.gt>
2025-06-04 21:09:59 -0600

    Hoja
    Se carga la codificación de hoja

--- src/hoja_calculo/Hoja.java
+++ src/hoja_calculo/Hoja.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hoja_calculo;
 
-/**
- *
- * @author Erick Camposeco
- */
+//Hoja de calculo
+
 public class Hoja {
-    
+
+    private int filas;
+    private int columnas;
+    private Celda[][] celdas;
+
+    public Hoja(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        celdas = new Celda[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                celdas[i][j] = new Celda(i, j, "", this);
+            }
+        }
+    }
+
+    public Celda getCelda(int fila, int columna) {
+        return celdas[fila][columna];
+    }
+
+    public Celda getCelda(String referencia) {
+        int columna = referencia.charAt(0) - 'A';
+        int fila = Integer.parseInt(referencia.substring(1)) - 1;
+        return getCelda(fila, columna);
+    }
+
+    public void setCelda(int fila, int columna, String contenido) {
+        celdas[fila][columna].setContenido(contenido);
+    }
+
+    public Celda[][] getCeldas() {
+        return celdas;
+    }
 }
